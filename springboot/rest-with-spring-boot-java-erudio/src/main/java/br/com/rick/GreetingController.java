3cbb4b4f33acaa411bfcbeb654bf20eac02f1507
monterick <rick_-26@hotmail.com>
2022-10-23 13:27:06 -0300

    'commit'

--- springboot/rest-with-spring-boot-java-erudio/src/main/java/br/com/rick/GreetingController.java
+++ springboot/rest-with-spring-boot-java-erudio/src/main/java/br/com/rick/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Nome: %s";
+	private static final String template = "Nomes: %s";
 	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping("/greeting")
